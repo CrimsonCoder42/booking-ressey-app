67c9da9907256ae6a76bb4ffbe494c8cbbee531c
CrimsonCoder42 <nostro37@gmail.com>
2022-05-17 09:24:53 -0400

    10:35

--- booking-app/src/components/navbar/navbar.js
+++ booking-app/src/components/navbar/navbar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './navbar.css'
 
 const navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Booker</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
+      </div>
+    </div>
     )
 }
 
--- booking-app/src/pages/Home/Home.js
+++ booking-app/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from "../../components/navbar/Navbar";
+
 import './Home.css'
 
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Navbar/>
         </div>
     )
 }
