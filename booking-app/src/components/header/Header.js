890e00c3ed18b644f97ba781d364df035581f5fb
CrimsonCoder42 <nostro37@gmail.com>
2022-05-17 12:05:52 -0400

    14:18

--- booking-app/src/components/header/Header.js
+++ booking-app/src/components/header/Header.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import "./Header.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerList">
                 <div className="headerListItem">
-                
+                <FontAwesomeIcon icon={"fa-solid fa-bed"} />
                 </div>
             </div>
         </div>
