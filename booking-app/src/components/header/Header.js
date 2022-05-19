3447ff61b5a6519345499360739a307efde6efb9
CrimsonCoder42 <nostro37@gmail.com>
2022-05-19 10:29:27 -0400

    18:55

--- booking-app/src/components/header/Header.js
+++ booking-app/src/components/header/Header.js
@@ -13,12 +13,38 @@ import {
 const Header = () => {
     return (
         <div className="header">
+        <div className="headerContainer">
             <div className="headerList">
+                <div className="headerListItem active">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+                </div>
+            
                 <div className="headerListItem">
-                <FontAwesomeIcon icon={"fa-solid fa-bed"} />
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+                </div>
+            
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car rentals</span>
+                </div>
+            
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Attractions</span>
+                </div>
+            
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport Taxi</span>
                 </div>
             </div>
+            <h1 className="headerTitle"> A Lifetime of discounts? It's Genius! </h1>
+            <p className="headerDesc"> Get rewarded for your travels - unlock instant savings of 10% or more with a free account. </p>
+            <button className="headerBtn"> Sign in / Register </button>
         </div>
+    </div>
     )
 }
 
